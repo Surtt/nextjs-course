af496029cb8b8b335645c060e2492ddc11436143
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2023-09-05 12:39:29 +0300

    animation

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { ButtonOrLink, Title, Typography } from '@/components';
 
 import styles from './card.module.css';
@@ -6,10 +8,16 @@ import ArrowIcon from '@/public/icons/arrow.svg';
 import Image from 'next/image';
 import { routes } from '@/constants/routes';
 import { Post } from '@/interfaces/post.interface';
+import { motion } from 'framer-motion';
 
 export const Card = ({ id, title, body }: Post) => {
   return (
-    <article className={styles.card}>
+    <motion.article
+      initial={{ marginLeft: '-100%' }}
+      transition={{ times: [0, 0.1, 1], duration: 1 }}
+      animate={{ marginLeft: '0%' }}
+      className={styles.card}
+    >
       <Image
         width={330}
         height={200}
@@ -37,6 +45,6 @@ export const Card = ({ id, title, body }: Post) => {
           Читать
         </ButtonOrLink>
       </div>
-    </article>
+    </motion.article>
   );
 };
